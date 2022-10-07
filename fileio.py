ee56fbd01480dfde1e0a30cc2fa1938bf6c228e4
Ashish kumar anand <ashishraj5296@gmail.com>
2022-10-07 17:02:28 +0530

    2nd

---
+++ fileio.py
@@ -0,0 +1,10 @@
+"""
+"r" = open file for reading
+"w" = open a file for writing
+"x" = creates file if not exists
+"a" = add more content to a file
+"t" = text mode
+"b" = binary mode
+"+" = read and write both
+"""
+
---
+++ filewriting.py
@@ -0,0 +1,7 @@
+f = open("ashish.txt","rt")
+content = f.read(3)
+print(content)
+content = f.read(3)
+print(content)
+f.close()
+hh
