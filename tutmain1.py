e3002f8d318f22220e5b1357183ec5f966774ac5
Ashish kumar anand <ashishraj5296@gmail.com>
2022-10-08 15:48:06 +0530

    naem equal to main

---
+++ tutmain1.py
@@ -0,0 +1,15 @@
+import sklearn
+def printaka(string):
+    return f"Ye string hamko dedo {string}"
+
+def add(num1,num2):
+    return num1+num2+5
+
+# print("and the name is",__name__)
+
+if __name__ == '__main__':
+    print(printaka("Ashish"))
+    o = add(4, 6)
+    print(o)
+
+
