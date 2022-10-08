2afa9efbfc080dbdfd59e54ab8a45b3cb90c6ffa
Ashish kumar anand <ashishraj5296@gmail.com>
2022-10-08 15:53:20 +0530

    join

---
+++ joinfunction.py
@@ -0,0 +1,10 @@
+# lst=["Ashish","subhash","gopal","prabhash","sakshi"]
+# for item in lst :
+#     print(item,"and",end=" ")
+# print("other brothers.")
+
+lst=["Ashish","subhash","gopal","prabhash","sakshi"]
+# for item in lst :
+#     print(item,"and",end=" ")
+a=" , ".join(lst)
+print(a,"and other brothers.")
