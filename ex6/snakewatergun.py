04446ebc61a25f5f172916e11e7bf4cfb0dfd6c1
Ashish kumar anand <ashishraj5296@gmail.com>
2022-10-08 16:25:01 +0530

    s,w,g updated

--- ex6/snakewatergun.py
+++ ex6/snakewatergun.py
@@ -1,35 +1,35 @@
 import random
-game =["snake","water","gun",]
+game =["s","w","g",]
 choice = random.choice(game)
 print("WELCOME TO SNAKE , WATER , GUN GAME 🙏")
 count=0
 countc=0
 num=1
 while(num<=10):
-    inp = input("Enter your choice (choices are snake , water , gun) :--> ")
-    if (choice == "snake" and inp == "gun"):
+    inp = input("Enter your choice (choices are s for snake , w for water , g for gun) :--> ")
+    if (choice == "s" and inp == "g"):
         print("You won")
         count += 1
-    elif (choice == "gun" and inp=="snake"):
+    elif (choice == "g" and inp=="s"):
         print("Computer won")
         countc += 1
-    elif (choice == "snake" and inp == "water"):
+    elif (choice == "s" and inp == "w"):
         print("You won")
         count += 1
-    elif (choice == "water" and inp == "snake"):
+    elif (choice == "w" and inp == "s"):
         print("Computer won")
         countc += 1
-    elif (choice == "water" and inp == "gun"):
+    elif (choice == "w" and inp == "g"):
         print("You won")
         count += 1
-    elif (choice == "gun" and inp == "water"):
+    elif (choice == "g" and inp == "w"):
         print("Computer won")
         countc += 1
-    elif(choice=="gun" and inp=="gun"):
+    elif(choice=="g" and inp=="g"):
         print("game die")
-    elif (choice == "water" and inp == "water"):
+    elif (choice == "w" and inp == "w"):
         print("game die")
-    elif (choice == "snake" and inp == "snake"):
+    elif (choice == "s" and inp == "s"):
         print("game die")
     else:
         print("Wrong choice entered")
@@ -38,6 +38,8 @@ while(num<=10):
 if(count>countc):
     total=count-countc
     print(f"You won by {total} points ",)
+elif(count==countc):
+    print("GAME TIE OR ERROR OCCURED DUE TO WRONG OPTION SELECTED")
 else:
     total = countc - count
     print(f"Computer won by {total} points ", )
