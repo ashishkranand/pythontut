7c95b9ba63113b8216add50fa4d9b9f63d004538
Ashish kumar anand <ashishraj5296@gmail.com>
2022-10-08 13:07:33 +0530

    modules

---
+++ using.py
@@ -0,0 +1,9 @@
+import random
+import sklearn
+random_num = random.randint(0,5)
+# print(random_num)
+rand= random.random()*100
+# print (rand)
+lst= ["star plus","DD1","Strar utsav","ABP news"]
+choice = random.choice(lst)
+print(choice)
