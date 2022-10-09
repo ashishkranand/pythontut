f2760c2135e1a3aa4156a1b9ffb21b12e1b56da3
Ashish kumar anand <ashishraj5296@gmail.com>
2022-10-09 20:19:45 +0530

    constructor

--- oops/oops3.py
+++ oops/oops3.py
@@ -1,17 +1,22 @@
 class Employee:
     no_of_leaves = 8
 
+    def __init__(self,aname , asalary, arole):
+        self.name=aname
+        self.salary=asalary
+        self.role=arole
+
     def printdetails(self):
         return  f"Name is {self.name}. Salary is {self.salary} andn role is {self.role}"
 
-ashish = Employee()
-aklesh = Employee()
+ashish = Employee("Ashish",125,"Instructor")
+# aklesh = Employee()
 
-ashish.name= "Ashish"
-ashish.salary = 1234
-ashish.role = "Instructor"
-aklesh.name = "Aklesh"
-aklesh.salary = 345654
-aklesh.role = "student"
+# ashish.name= "Ashish"
+# ashish.salary = 1234
+# ashish.role = "Instructor"
+# aklesh.name = "Aklesh"
+# aklesh.salary = 345654
+# aklesh.role = "student"
 
 print(ashish.printdetails())
