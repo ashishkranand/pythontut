edb1e971acf21101f14a8c7b50bbfa60b7c24e66
Ashish kumar anand <ashishraj5296@gmail.com>
2022-10-09 19:58:50 +0530

    oops1

---
+++ oops/oops1.py
@@ -0,0 +1,11 @@
+class student:
+    pass
+
+ashish = student()
+aklesh = student()
+ashish.name = "Ashish"
+ashish.std = "MCA"
+ashish.section = "D2113"
+aklesh.name = "Aklesh"
+ashish.subjects = ["hindi","English"]
+print(ashish.name, ashish.subjects)
